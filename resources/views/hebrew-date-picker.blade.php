d371ac208454243e66b6ae02ce275e671a62a1b1
StaffCollab <ydwiznitzer@gmail.com>
2024-05-01 21:48:44 -0400

    Quick fix

---
+++ resources/views/hebrew-date-picker.blade.php
@@ -0,0 +1,3 @@
+<div>
+    Hello from hebrew-date-picker.blade.php
+</div>
---
+++ src/HebrewDatePicker.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace StaffCollab\HebrewDate;
+
+use Filament\Forms\Components\Field;
+
+class HebrewDatePicker extends Field
+{
+    public $name = 'hebrew-date-picker';
+
+    public $format = 'YYYY-MM-DD';
+
+    public $type = 'text';
+
+    public $view = 'staffcollab::hebrew-date-picker';
+
+    public function format($format)
+    {
+        $this->format = $format;
+
+        return $this;
+    }
+
+    public function type($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+}
